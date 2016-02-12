e82dedd24fa0052f1ada2f11bf6d6801a3331afd
BenjiGrus <benji.grus@icloud.com>
2016-02-12 10:53:20 +0100

    add main menu

--- app/src/main/java/info/iut/acy/fr/miniproject/MainActivity.java
+++ app/src/main/java/info/iut/acy/fr/miniproject/MainActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainActivity extends Activity {
@@ -12,5 +17,25 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView MainMenu = (ListView)findViewById(R.id.MainMenuListView);
+
+        //remplis la arraylist avec la méthode fillHashMap
+        ArrayList<HashMap<String, String>> appItemList = new ArrayList<HashMap<String, String>>();
+        appItemList.add(fillHashMap("Mes entreprises", String.valueOf((R.drawable.entreprise))));
+
+        // Création d'un SimpleAdapter qui met en correspondance les items présents dans la list avec ceux de la vue
+        SimpleAdapter itemsAdapter = new SimpleAdapter(this.getBaseContext(), appItemList, R.layout.main_menu_item,
+                new String[]{"MenuTitle", "MenuIcon"}, new int[]{R.id.titre_menu, R.id.imageMenu});
+
+        MainMenu.setAdapter(itemsAdapter);
+    }
+
+    // méthode privée permettant de remplir un HashMap
+    private HashMap<String, String> fillHashMap (String Title, String icon){
+        HashMap<String, String> item = new HashMap<String, String>();
+        item.put("MenuTitle", Title);
+        item.put("MenuIcon", icon);
+        return item;
     }
 }
