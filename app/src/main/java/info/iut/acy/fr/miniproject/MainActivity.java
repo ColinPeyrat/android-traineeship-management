c5eddd40d00d4fabe1e1baea6543ad804efe451b
Colin Peyrat <contact@colin-peyrat.com>
2016-02-12 11:30:45 +0100

    add service in companyy

--- app/src/main/java/info/iut/acy/fr/miniproject/MainActivity.java
+++ app/src/main/java/info/iut/acy/fr/miniproject/MainActivity.java
@@ -18,20 +18,18 @@ public class MainActivity extends Activity {
 
         ListView MainMenu = (ListView)findViewById(R.id.MainMenuListView);
 
-        //remplis la arraylist avec la méthode fillHashMap
-        ArrayList<HashMap<String, String>> menuItemList = new ArrayList<HashMap<String, String>>();
-        menuItemList.add(fillHashMap("Mes offres de stage", String.valueOf((R.drawable.company))));
-        menuItemList.add(fillHashMap("Mes contacts",String.valueOf(R.drawable.company)));
-        menuItemList.add(fillHashMap("Mes information",String.valueOf(R.drawable.company)));
+        //remplis la arraylist avec la mÃ©thode fillHashMap
+        ArrayList<HashMap<String, String>> appItemList = new ArrayList<HashMap<String, String>>();
+        appItemList.add(fillHashMap("Mes entreprises", String.valueOf((R.drawable.company))));
 
-        // Création d'un SimpleAdapter qui met en correspondance les items présents dans la list avec ceux de la vue
-        SimpleAdapter itemsAdapter = new SimpleAdapter(this.getBaseContext(), menuItemList, R.layout.main_menu_item,
+        // CrÃ©ation d'un SimpleAdapter qui met en correspondance les items prÃ©sents dans la list avec ceux de la vue
+        SimpleAdapter itemsAdapter = new SimpleAdapter(this.getBaseContext(), appItemList, R.layout.main_menu_item,
                 new String[]{"MenuTitle", "MenuIcon"}, new int[]{R.id.titre_menu, R.id.imageMenu});
 
         MainMenu.setAdapter(itemsAdapter);
     }
 
-    // méthode privée permettant de remplir un HashMap
+    // mÃ©thode privÃ©e permettant de remplir un HashMap
     private HashMap<String, String> fillHashMap (String Title, String icon){
         HashMap<String, String> item = new HashMap<String, String>();
         item.put("MenuTitle", Title);
