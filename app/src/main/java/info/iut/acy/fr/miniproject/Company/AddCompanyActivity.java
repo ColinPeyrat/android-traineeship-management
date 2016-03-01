46149d711c282067fa8e20f2ce93776b03785de0
Colin Peyrat <contact@colin-peyrat.com>
2016-03-01 17:36:46 +0100

    add form validation for adding a company

--- app/src/main/java/info/iut/acy/fr/miniproject/Company/AddCompanyActivity.java
+++ app/src/main/java/info/iut/acy/fr/miniproject/Company/AddCompanyActivity.java
@@ -10,15 +10,36 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import info.iut.acy.fr.miniproject.Database.TraineeshipAdapter;
 import info.iut.acy.fr.miniproject.R;
 
 
 public class AddCompanyActivity extends Activity {
 
+    private EditText companyNameEditText;
+    private EditText companyAdressEditText;
+    private EditText companyPostalEditText;
+    private EditText companyCityEditText;
+    private EditText companyCountryEditText;
+    private EditText companyPhoneEditText;
+    private EditText companyMailEditText;
+    private EditText companyWebSiteEditText;
+    private EditText companySizeEditText;
+    private EditText companyDescriptionEditText;
+
+
+    TraineeshipAdapter TraineeshipDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_company);
+
+        TraineeshipDB = new TraineeshipAdapter(getApplicationContext());
+
 
         Button btnSubmint = (Button)findViewById(R.id.submitCompany);
         btnSubmint.setOnClickListener(new OnClickListener() {
@@ -26,26 +47,85 @@ public class AddCompanyActivity extends Activity {
             public void onClick(View v) {
 
 
-                EditText companyName = (EditText)findViewById(R.id.EditTextName);
-                EditText companyMail = (EditText)findViewById(R.id.EditTextMail);
-                EditText companyPhone = (EditText)findViewById(R.id.EditTextPhone);
+                companyNameEditText = (EditText)findViewById(R.id.EditTextName);
+                companyAdressEditText = (EditText)findViewById(R.id.EditTextAdresse);
+                companyPostalEditText = (EditText)findViewById(R.id.EditTextPostal);
+                companyCityEditText = (EditText)findViewById(R.id.EditTextCity);
+                companyCountryEditText = (EditText)findViewById(R.id.EditTextCountry);
+                companyMailEditText = (EditText)findViewById(R.id.EditTextMail);
+                companyPhoneEditText = (EditText)findViewById(R.id.EditTextPhone);
 
-                if( companyName.getText().toString().trim().equals("")) {
-                    companyName.setError("Le nom de l'entreprise est requis");
+                String companyName = companyNameEditText.getText().toString();
+                String companyAdress = companyAdressEditText.getText().toString();
+                String companyPostal = companyPostalEditText.getText().toString();
+                String companyCity = companyCityEditText.getText().toString();
+                String companyCountry = companyCountryEditText.getText().toString();
+                String companyPhone = companyPhoneEditText.getText().toString();
+                String companyMail = companyMailEditText.getText().toString();
 
-                // verify if one of phone or mail is not empty
-                } else if(companyPhone.getText().toString().trim().equals("")  || companyMail.getText().toString().trim().equals("")){
+                if( companyName.trim().equals("")) {
+                    companyNameEditText.setError("Le nom de l'entreprise est requis");
 
-                    Toast.makeText(getApplicationContext(), "contact", Toast.LENGTH_LONG).show();
+                } else if(companyAdress.trim().equals("")) {
+                    companyAdressEditText.setError("L'adresse est obligatoire");
 
+                } else if(companyPostal.trim().equals("")) {
+                    companyPostalEditText.setError("Le code postal est obligatoire");
+                } else if(!isValidPostalCode(companyPostal)){
+                    companyPostalEditText.setError("Le code postal est invalide");
+
+                } else if(companyCity.trim().equals("")) {
+                    companyCityEditText.setError("La ville est obligatoire");
+
+                } else if(companyCountry.trim().equals("")){
+                    companyCountryEditText.setError("Le pays est obligatoire");
+
+                    // verify if less one of phone or mail is not empty
+                } else if(companyPhone.trim().equals("")  && companyMail.trim().equals("")) {
+                    companyPhoneEditText.setError("Vous devez renseigner au moins un champ de contact");
+                    companyMailEditText.setError("Vous devez renseigner au moins un champ de contact");
+                } else if(!companyPhone.trim().equals("") && !isValidPhoneNumber(companyPhone)){
+                    companyPhoneEditText.setError("Numéro de télephone invalide");
+                    companyMailEditText.setError(null);
+
+                } else if(!companyMail.trim().equals("") && !isValidEmail(companyMail)){
+                    companyPhoneEditText.setError(null);
+                    companyMailEditText.setError("Email invalide");
                 }else{
                     Toast.makeText(getApplicationContext(), "C'est bon zinc", Toast.LENGTH_LONG).show();
 
+                    //remove all error
+                    companyPhoneEditText.setError(null);
+                    companyMailEditText.setError(null);
                 }
 
 
             }
         });
+    }
+    // validating email id
+    private boolean isValidEmail(String email) {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    // validating postal
+    private boolean isValidPostalCode(String postal) {
+        String POSTAL_PATTERN = "^[0-9]{5,5}$";
+        Pattern pattern = Pattern.compile(POSTAL_PATTERN);
+        Matcher matcher = pattern.matcher(postal);
+        return matcher.matches();
+    }
+    //validating phone number
+    private boolean isValidPhoneNumber(String phone) {
+        String POSTAL_PATTERN = "^(\\d\\d){5}$";
+        Pattern pattern = Pattern.compile(POSTAL_PATTERN);
+        Matcher matcher = pattern.matcher(phone);
+        return matcher.matches();
     }
 
 //    @Override
