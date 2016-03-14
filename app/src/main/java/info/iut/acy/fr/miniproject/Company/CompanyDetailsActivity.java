00788ad270fcd3b2b59160c9eec41c4839dc0e1f
Colin Peyrat <contact@colin-peyrat.com>
2016-03-14 09:01:10 +0100

    refactoring some ugly code

--- app/src/main/java/info/iut/acy/fr/miniproject/Company/CompanyDetailsActivity.java
+++ app/src/main/java/info/iut/acy/fr/miniproject/Company/CompanyDetailsActivity.java
@@ -1,29 +1,51 @@
 package info.iut.acy.fr.miniproject.Company;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import info.iut.acy.fr.miniproject.Database.TraineeshipAdapter;
 import info.iut.acy.fr.miniproject.R;
 
 public class CompanyDetailsActivity extends Activity {
+
+    TraineeshipAdapter TraineeshipDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_details);
 
-        Long data = null;
+        TraineeshipDB = new TraineeshipAdapter(getApplicationContext());
+
+        TraineeshipDB.open();
+
+        TextView tvTest = (TextView)findViewById(R.id.test);
+
+        Long idCompany = null;
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
-             data = extras.getLong("idCompany");
+            idCompany = extras.getLong("idCompany");
         }
-        TextView tvTest = (TextView)findViewById(R.id.test);
-        tvTest.setText(String.valueOf(data));
+        Cursor company = TraineeshipDB.getSingleCompany(idCompany);
+        if(company.getCount() > 0){
+            company.moveToFirst();
+            tvTest.setText(company.getString(company.getColumnIndex(TraineeshipDB.KEY_NAME)));
+        } else {
+            finish();
+            Toast.makeText(getApplicationContext(), "Impossible de récuperer l'entreprise", Toast.LENGTH_SHORT).show();
+        }
 
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
